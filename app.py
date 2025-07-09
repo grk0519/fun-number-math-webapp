b3a90e8fb7b26a43416de92e9fd2d41a56da9d36
Raja kumar <grk0519@gmail.com>
2025-07-09 12:41:10 +0530

    initial commit for fun number math app

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def get_number_details(n):
+    return {
+        'multiplication': [f"{n} x {i} = {n*i}" for i in range(1, 11)],
+        'is_prime': n > 1 and all(n % i != 0 for i in range(2, int(n**0.5)+1)),
+        'factors': [i for i in range(1, n+1) if n % i == 0],
+        'square': n ** 2,
+        'cube': n ** 3,
+        'even_or_odd': "Even" if n % 2 == 0 else "Odd"
+    }
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    number = int(request.form['number'])
+    details = get_number_details(number)
+    return render_template('result.html', number=number, details=details)
+
+if __name__ == '__main__':
+    app.run(debug=True)
